af4cf6fba5e07241376e0dae8617d7a1028af484
qnra <111017422+qnra@users.noreply.github.com>
2026-05-30 11:07:13 +0200

    Delete sensor directory

--- sensor/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Pomiar(models.Model):
-    distance = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.distance} cm"
